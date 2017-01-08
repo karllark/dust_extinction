71f70d26e7e04aad609e8eeb9a2326d25d6c29d0
Karl Gordon <kgordon@stsci.edu>
2017-01-08 15:27:03 -0500

    Working CCM89 code

--- dust_extinction/ccm89.py
+++ dust_extinction/ccm89.py
@@ -2,31 +2,77 @@
 # some code adapted from google code extinct.py by Erik Tollerud
 #  adaption needed as we are directly interested in the extinction curve
 #  not in dereddening a spectrum (forward instead of reverse modeling)
-#  and some changes were needed to the limits
 
+from __future__ import print_function, division
+
+import string
 import numpy as np
 
-from astropy.modeling import Model, Parameter
+from astropy.modeling import Model, Parameter, InputParameterError
+
+__all__ = ['CCM89']
 
 class CCM89(Model):
+    """
+    CCM89 extinction model calculation
+
+    Parameters
+    ----------
+    x: float
+        wavelengths in wavenumbers [1/micron]
+
+    Rv: float
+        R(V) = A(V)/E(B-V) = total-to-selective extinction
+
+    Returns
+    -------
+    elvebv: float
+        E(x-V)/E(B-V) extinction curve [mag]
+
+    Notes
+    -----
+    CCM89 Milky Way R(V) dependent extinction model
+
+    From Cardelli, Clayton, and Mathis (1989, ApJ, 345, 245)
+
+    F99 should be used instead as it is based on 10x more observations 
+    and a better treatment of the optical/NIR photometry based portion 
+    of the curves.
+
+    CCM89 is mainly for historical puposes
+    """
+    inputs = ('x',)
+    outputs = ('elvebv',)
     Rv = Parameter(description="R(V) = A(V)/E(B-V) = " \
                    + "total-to-selective extinction",
-                   default=3.1,
-                   min=2.0, max=6.0)
-    #Rv_range = [2.0,6.0]
+                   default=3.1)
+    Rv_range = [2.0,6.0]
 
-    #@Rv.validator
-    #def Rv(self, value):
-    #    if not (Rv_range[0] <= value <= Rv_range[1]):
-    #        raise InputParameterError("parameter Rv must be between "
-    #                                  + string(Rv_range[0])
-    #                                  + " and "
-    #                                  + string(Rv_range[1])
-    #                                  + '.')
+    @Rv.validator
+    def Rv(self, value):
+        """
+        Check that Rv is in the valid range
+
+        Parameters
+        ----------
+        value: float
+            R(V) value to check
+
+        Returns
+        -------
+        Raises expection if it is not between the bounds defined by Rv_range
+        """
+        if not (self.Rv_range[0] <= value <= self.Rv_range[1]):
+            raise InputParameterError("parameter Rv must be between "
+                                      + str(self.Rv_range[0])
+                                      + " and "
+                                      + str(self.Rv_range[1]))
     
     @staticmethod
     def evaluate(x, Rv):
-        
+        """
+        CCM89 model function.
+        """
         n_x = len(x)
         a = np.zeros(n_x)
         b = np.zeros(n_x)
@@ -67,8 +113,6 @@ class CCM89(Model):
         a[fuv_indxs] = np.polyval((-.070, .137, -.628, -1.073), y)
         b[fuv_indxs] = np.polyval((.374, -.42, 4.257, 13.67), y)
 
-        print(a,b)
-        
         # return Al/Av
         return a + b/Rv
         
