965a409be40435cc2eb19523501f22a3b305492f
Karl Gordon <kgordon@stsci.edu>
2017-09-11 11:48:39 -0400

    FM90 fully fittable including test code

--- dust_extinction/tests/test_fm90.py
+++ dust_extinction/tests/test_fm90.py
@@ -3,8 +3,9 @@ import pytest
 
 import astropy.units as u
 from astropy.modeling import InputParameterError
+from astropy.modeling.fitting import LevMarLSQFitter
 
-from ..dust_extinction import FM90
+from ..dust_extinction import FM90, G03_LMCAvg
 
 x_bad = [-1.0, 0.2, 3.0, 11.0, 100.]
 @pytest.mark.parametrize("x_invalid", x_bad)
@@ -78,3 +79,26 @@ def test_extinction_FM90_values():
 
     # test
     np.testing.assert_allclose(tmodel(x), cor_vals)
+
+
+def test_FM90_fitting():
+
+    # get an observed extinction curve to fit
+    g03_model = G03_LMCAvg()
+
+    x = g03_model.obsdata_x
+    # convert to E(x-V)/E(B0V)
+    y = (g03_model.obsdata_axav - 1.0)*g03_model.Rv
+    # only fit the UV portion (FM90 only valid in UV)
+    gindxs, = np.where(x > 3.125)
+    
+    fm90_init = FM90()
+    fit = LevMarLSQFitter()    
+    g03_fit = fit(fm90_init, x[gindxs], y[gindxs])
+    fit_vals = [g03_fit.C1.value, g03_fit.C2.value, g03_fit.C3.value,
+                g03_fit.C4.value, g03_fit.xo.value, g03_fit.gamma.value]
+    
+    good_vals = np.array([-0.958016797002, 1.0109751831, 2.96430606652,
+                          0.313137860902, 4.59996300532, 0.99000982258])
+
+    np.testing.assert_allclose(good_vals, fit_vals)
