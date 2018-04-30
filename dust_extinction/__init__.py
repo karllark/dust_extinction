b364a8722d5d18c631706b56ef964d3d073bedfc
Karl Gordon <kgordon@stsci.edu>
2018-04-30 14:28:25 -0400

    Updating using the new package template via cookiecutter

--- dust_extinction/__init__.py
+++ dust_extinction/__init__.py
@@ -1,16 +1,23 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-"""
-This is an Astropy affiliated package.
-"""
-
-# Affiliated packages may add whatever they like to this file, but
+# Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
 from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
+# Enforce Python version check during package import.
+# This is the same check as the one at the top of setup.py
+import sys
+
+class UnsupportedPythonError(Exception):
     pass
-    #    from .example_mod import *
+
+if sys.version_info < tuple((int(val) for val in "2.7".split('.'))):
+    raise UnsupportedPythonError("dust_extinction does not support Python < {}".format(2.7))
+
+if not _ASTROPY_SETUP_:
+    # For egg_info test builds to pass, put package imports here.
+
+    pass
+
