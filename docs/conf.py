50de616b8eb8063e86b5beb5b28408584935b421
Karl Gordon <kgordon@stsci.edu>
2018-04-30 15:55:57 -0400

    Update to keep the package name in the docs

--- docs/conf.py
+++ docs/conf.py
@@ -115,8 +115,8 @@ release = package.__version__
 
 # Please update these texts to match the name of your package.
 html_theme_options = {
-    'logotext1': 'package',  # white,  semi-bold
-    'logotext2': '-template',  # orange, light
+    'logotext1': 'dust',  # white,  semi-bold
+    'logotext2': '_extinction',  # orange, light
     'logotext3': ':docs'   # white,  light
     }
 
