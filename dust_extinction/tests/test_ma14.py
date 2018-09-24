ba63cd97676dd8c6089207da3aa99e7edd9639a2
krislars <krislars@gmail.com>
2018-09-23 20:37:24 -0700

    change MA14 to M14, fix order

--- dust_extinction/tests/test_ma14.py
+++ dust_extinction/tests/test_ma14.py
@@ -3,7 +3,7 @@ import pytest
 
 import astropy.units as u
 
-from ..parameter_averages import MA14
+from ..parameter_averages import M14
 from .helpers import _invalid_x_range
 
 
@@ -12,22 +12,22 @@ x_bad = [-1.0, 0.1, 12.0, 100.]
 
 @pytest.mark.parametrize("x_invalid", x_bad)
 def test_invalid_wavenumbers(x_invalid):
-    _invalid_x_range(x_invalid, MA14(), 'MA14')
+    _invalid_x_range(x_invalid, M14(), 'M14')
 
 
 @pytest.mark.parametrize("x_invalid_wavenumber", x_bad/u.micron)
 def test_invalid_wavenumbers_imicron(x_invalid_wavenumber):
-    _invalid_x_range(x_invalid_wavenumber, MA14(), 'MA14')
+    _invalid_x_range(x_invalid_wavenumber, M14(), 'M14')
 
 
 @pytest.mark.parametrize("x_invalid_micron", u.micron/x_bad)
 def test_invalid_micron(x_invalid_micron):
-    _invalid_x_range(x_invalid_micron, MA14(), 'MA14')
+    _invalid_x_range(x_invalid_micron, M14(), 'M14')
 
 
 @pytest.mark.parametrize("x_invalid_angstrom", u.angstrom*1e4/x_bad)
 def test_invalid_micron(x_invalid_angstrom):
-    _invalid_x_range(x_invalid_angstrom, MA14(), 'MA14')
+    _invalid_x_range(x_invalid_angstrom, M14(), 'M14')
 
 
 def get_axav_cor_vals():
@@ -45,12 +45,12 @@ def get_axav_cor_vals():
     return (x, cor_vals, tolerance)
 
 
-def test_extinction_MA14_values():
+def test_extinction_M14_values():
     # get the correct values
     x, cor_vals, tolerance = get_axav_cor_vals()
 
     # initialize extinction model
-    tmodel = MA14()
+    tmodel = M14()
 
     # test
     np.testing.assert_allclose(tmodel(x), cor_vals, rtol=tolerance)
@@ -61,11 +61,11 @@ test_vals = zip(x_vals, axav_vals, np.full(len(x_vals), tolerance))
 
 
 @pytest.mark.parametrize("test_vals", test_vals)
-def test_extinction_MA14_single_values(test_vals):
+def test_extinction_M14_single_values(test_vals):
     x, cor_val, tolerance = test_vals
 
     # initialize extinction model
-    tmodel = MA14()
+    tmodel = M14()
 
     # test
     np.testing.assert_allclose(tmodel(x), cor_val, rtol=tolerance)
