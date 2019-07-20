371cc5fde98e19b1485fdc9edeacea38376da4f3
Karl Gordon <kgordon@stsci.edu>
2019-07-20 14:09:28 -0400

    nounits test now pass for all models

--- dust_extinction/tests/test_nounits.py
+++ dust_extinction/tests/test_nounits.py
@@ -15,9 +15,29 @@ from dust_extinction.parameter_averages import (
     G16,
     F20,
 )
+from dust_extinction.shapes import FM90, P92
+from dust_extinction.averages import G03_SMCBar, G03_LMCAvg, G03_LMC2, GCC09_MWAvg
+
+all_models = [
+    CCM89,
+    O94,
+    F99,
+    F04,
+    VCG04,
+    GCC09,
+    M14,
+    G16,
+    F20,
+    FM90,
+    P92,
+    G03_SMCBar,
+    G03_LMCAvg,
+    G03_LMC2,
+    GCC09_MWAvg,
+]
 
 
-@pytest.mark.parametrize("model", [CCM89, O94, F99, F04, VCG04, GCC09, M14, G16, F20])
+@pytest.mark.parametrize("model", all_models)
 def test_nounits_warning(model):
     ext = model()
     x = np.arange(ext.x_range[0], ext.x_range[1], 0.1)
@@ -28,7 +48,7 @@ def test_nounits_warning(model):
         ext(x)
 
 
-@pytest.mark.parametrize("model", [CCM89, O94, F99, F04, VCG04, GCC09, M14, G16, F20])
+@pytest.mark.parametrize("model", all_models)
 def test_units_nowarning_expected(model):
     ext = model()
     x = [0.5 * (ext.x_range[0] + ext.x_range[1])] / u.micron
