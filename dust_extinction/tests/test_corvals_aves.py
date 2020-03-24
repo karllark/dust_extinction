de5f20571c4ec903783c8510971ed04e53ed7ff5
Karl Gordon <kgordon@stsci.edu>
2020-03-24 11:40:14 -0400

    More test consolidation

--- dust_extinction/tests/test_corvals_aves.py
+++ dust_extinction/tests/test_corvals_aves.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import astropy.units as u
 
 from dust_extinction.averages import (
     RL85_MWAvg,
@@ -27,3 +28,21 @@ def test_corvals(model_class):
     for x, y in zip(x_vals, y_vals):
         np.testing.assert_allclose(tmodel(x), y, rtol=tol)
         np.testing.assert_allclose(tmodel.evaluate(x), y, rtol=tol)
+
+
+@pytest.mark.parametrize("model_class", models)
+def test_extinguish_values_Av_or_Ebv(model_class):
+    ext = model_class()
+    x = np.arange(ext.x_range[0], ext.x_range[1]) / u.micron
+    cor_axav = ext(x)
+
+    # calculate the cor_vals in fractional units
+    Av = 1.0
+    cor_vals_av = np.power(10.0, -0.4 * (cor_axav * Av))
+    Ebv = 1.0
+    Av_ebv = Ebv * ext.Rv
+    cor_vals_ebv = np.power(10.0, -0.4 * cor_axav * Av_ebv)
+
+    # tests
+    np.testing.assert_equal(ext.extinguish(x, Av=Av), cor_vals_av)
+    np.testing.assert_equal(ext.extinguish(x, Ebv=Ebv), cor_vals_ebv)
