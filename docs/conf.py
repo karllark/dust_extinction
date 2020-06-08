325486911db6c13794166efc1f7a1ff98b5de1c8
Karl Gordon <kgordon@stsci.edu>
2020-06-08 12:29:54 -0400

    typo

--- docs/conf.py
+++ docs/conf.py
@@ -192,4 +192,4 @@ github_issues_url = 'https://github.com/{0}/issues/'.format(setup_cfg['github_pr
 #     target = target.strip()
 #     nitpick_ignore.append((dtype, six.u(target)))
 
-extensions += [''sphinx.ext.graphviz']
+extensions += ['sphinx.ext.graphviz']
