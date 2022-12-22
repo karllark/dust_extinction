bf92c3363e23ba9d341aedba55dd3f3467012c59
Karl D. Gordon <kgordon@stsci.edu>
2022-12-22 13:36:50 -0500

    updating coefficients (and black autoformater changes)

--- dust_extinction/parameter_averages.py
+++ dust_extinction/parameter_averages.py
@@ -143,8 +143,8 @@ class CCM89(BaseExtRvModel):
 
         # far-NUV
         y = x[fnuv_indxs] - 5.9
-        a[fnuv_indxs] += -0.04473 * (y ** 2) - 0.009779 * (y ** 3)
-        b[fnuv_indxs] += 0.2130 * (y ** 2) + 0.1207 * (y ** 3)
+        a[fnuv_indxs] += -0.04473 * (y**2) - 0.009779 * (y**3)
+        b[fnuv_indxs] += 0.2130 * (y**2) + 0.1207 * (y**3)
 
         # FUV
         y = x[fuv_indxs] - 8.0
@@ -269,8 +269,8 @@ class O94(BaseExtRvModel):
 
         # far-NUV
         y = x[fnuv_indxs] - 5.9
-        a[fnuv_indxs] += -0.04473 * (y ** 2) - 0.009779 * (y ** 3)
-        b[fnuv_indxs] += 0.2130 * (y ** 2) + 0.1207 * (y ** 3)
+        a[fnuv_indxs] += -0.04473 * (y**2) - 0.009779 * (y**3)
+        b[fnuv_indxs] += 0.2130 * (y**2) + 0.1207 * (y**3)
 
         # FUV
         y = x[fuv_indxs] - 8.0
@@ -394,7 +394,7 @@ class F99(BaseExtRvModel):
                 -0.426 + 1.0044 * Rv,
                 -0.050 + 1.0016 * Rv,
                 0.701 + 1.0016 * Rv,
-                1.208 + 1.0032 * Rv - 0.00033 * (Rv ** 2),
+                1.208 + 1.0032 * Rv - 0.00033 * (Rv**2),
             ]
         )
         nir_axebv_y = np.array([0.265, 0.829]) * Rv / 3.1
@@ -530,11 +530,11 @@ class F04(BaseExtRvModel):
                 -0.426 + 1.0044 * Rv,
                 -0.050 + 1.0016 * Rv,
                 0.701 + 1.0016 * Rv,
-                1.208 + 1.0032 * Rv - 0.00033 * (Rv ** 2),
+                1.208 + 1.0032 * Rv - 0.00033 * (Rv**2),
             ]
         )
         # updated NIR curve from F04, note R dependence
-        nir_axebv_y = (0.63 * Rv - 0.84) * nir_axav_x ** 1.84
+        nir_axebv_y = (0.63 * Rv - 0.84) * nir_axav_x**1.84
 
         optnir_axebv_y = np.concatenate([nir_axebv_y, opt_axebv_y])
 
@@ -658,8 +658,8 @@ class VCG04(BaseExtRvModel):
 
         # far-NUV
         y = x[fnuv_indxs] - 5.9
-        a[fnuv_indxs] += -0.0077 * (y ** 2) - 0.0030 * (y ** 3)
-        b[fnuv_indxs] += 0.2060 * (y ** 2) + 0.0550 * (y ** 3)
+        a[fnuv_indxs] += -0.0077 * (y**2) - 0.0030 * (y**3)
+        b[fnuv_indxs] += 0.2060 * (y**2) + 0.0550 * (y**3)
 
         # return A(x)/A(V)
         return a + b / Rv
@@ -769,8 +769,8 @@ class GCC09(BaseExtRvModel):
 
         # far-NUV
         y = x[fnuv_indxs] - 5.9
-        a[fnuv_indxs] += -0.110 * (y ** 2) - 0.0100 * (y ** 3)
-        b[fnuv_indxs] += 0.531 * (y ** 2) + 0.0544 * (y ** 3)
+        a[fnuv_indxs] += -0.110 * (y**2) - 0.0100 * (y**3)
+        b[fnuv_indxs] += 0.531 * (y**2) + 0.0544 * (y**3)
 
         # return A(x)/A(V)
         return a + b / Rv
@@ -880,8 +880,8 @@ class M14(BaseExtRvModel):
         Rv = Rv[0]
 
         # Infrared
-        ai = 0.574 * x ** 1.61
-        bi = -0.527 * x ** 1.61
+        ai = 0.574 * x**1.61
+        bi = -0.527 * x**1.61
 
         # Optical
         x1 = np.array([1.0])
@@ -890,10 +890,10 @@ class M14(BaseExtRvModel):
         x3 = np.array([3.5, 3.9, 4.0, 4.1, 4.2])
         xi3 = x3[-1]
 
-        a1v = 0.574 * x1 ** 1.61
-        a1d = 0.574 * 1.61 * xi1 ** 0.61
-        b1v = -0.527 * x1 ** 1.61
-        b1d = -0.527 * 1.61 * xi1 ** 0.61
+        a1v = 0.574 * x1**1.61
+        a1d = 0.574 * 1.61 * xi1**0.61
+        b1v = -0.527 * x1**1.61
+        b1d = -0.527 * 1.61 * xi1**0.61
 
         a2v = (
             1
@@ -1447,11 +1447,11 @@ class G23(BaseExtRvModel):
         # NIR/MIR
         # fmt: off
         # (scale, alpha1, alpha2, swave, swidth), sil1, sil2
-        ir_a = [0.37613, 1.78389, 1.57857, 2.25122, 1.73941,
-                0.06769, 9.89753, 2.63511, -0.12937,
-                0.03136, 19.21582, 17., -0.27]
-        ir_b = [-0.9765, 1., -1.07849]
+        ir_a = [0.38526, 1.68467, 0.78791, 4.30578, 4.78338,
+                0.06652, 9.8434, 2.21205, -0.24703,
+                0.0267 , 19.58294, 17., -0.27]
         # fmt: on
+        ir_b = [-1.01251, 1.0, -1.06099]
         self.a[ir_indxs] = self.nirmir_intercept(x[ir_indxs], ir_a)
 
         irpow = PowerLaw1D()
@@ -1461,14 +1461,14 @@ class G23(BaseExtRvModel):
         # optical
         # fmt: off
         # polynomial coeffs, ISS1, ISS2, ISS3
-        opt_a = [-0.43209, 0.83726, 0.00863, -0.03223, 0.00456,
-                 0.03807, 2.288, 0.243,
-                 0.03045, 2.054, 0.179,
-                 0.01584, 1.587, 0.243]
-        opt_b = [0.09955, -2.39681, 1.6707, -0.25078, 0.01491,
-                 0.17054, 2.288, 0.243,
-                 0.25673, 2.054, 0.179,
-                 0.10227, 1.587, 0.243]
+        opt_a = [-0.35848, 0.7122 , 0.08746, -0.05403, 0.00674,
+                 0.03893, 2.288, 0.243,
+                 0.02965, 2.054, 0.179,
+                 0.01747, 1.587, 0.243]
+        opt_b = [0.12354, -2.68335, 2.01901, -0.39299, 0.03355,
+                 0.18453, 2.288, 0.243,
+                 0.19728, 2.054, 0.179,
+                 0.1713 , 1.587, 0.243]
         # fmt: on
         m20_model_a = Polynomial1D(4) + Drude1D() + Drude1D() + Drude1D()
         m20_model_a.parameters = opt_a
@@ -1482,7 +1482,7 @@ class G23(BaseExtRvModel):
         #     1.0 / optir_waves[1] - 1.0 / optir_waves[0]
         # )
         weights = _smoothstep(
-            1. / x[optir_overlap], x_min=optir_waves[1], x_max=optir_waves[0], N=1
+            1.0 / x[optir_overlap], x_min=optir_waves[1], x_max=optir_waves[0], N=1
         )
         self.a[optir_overlap] = weights * m20_model_a(x[optir_overlap])
         self.a[optir_overlap] += (1.0 - weights) * self.nirmir_intercept(
@@ -1492,8 +1492,8 @@ class G23(BaseExtRvModel):
         self.b[optir_overlap] += (1.0 - weights) * irpow(x[optir_overlap])
 
         # Ultraviolet
-        uv_a = [0.81971, 0.27923, 1.02835, 0.11024, 4.6, 0.99002]
-        uv_b = [-2.79763, 1.85151, 3.42239, 0.65447, 4.60002, 0.99008]
+        uv_a = [0.81297, 0.2775, 1.06295, 0.11303, 4.59999, 0.99003]
+        uv_b = [-2.97868, 1.89808, 3.10334, 0.65484, 4.60002, 0.99013]
 
         fm90_model_a = FM90()
         fm90_model_a.parameters = uv_a
@@ -1507,7 +1507,7 @@ class G23(BaseExtRvModel):
         #     1.0 / uvopt_waves[1] - 1.0 / uvopt_waves[0]
         # )
         weights = _smoothstep(
-            1. / x[uvopt_overlap], x_min=uvopt_waves[1], x_max=uvopt_waves[0], N=1
+            1.0 / x[uvopt_overlap], x_min=uvopt_waves[1], x_max=uvopt_waves[0], N=1
         )
         self.a[uvopt_overlap] = weights * fm90_model_a(x[uvopt_overlap] / u.micron)
         self.a[uvopt_overlap] += (1.0 - weights) * m20_model_a(x[uvopt_overlap])
