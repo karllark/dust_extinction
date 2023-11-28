0b2ddef0b8351aebcf3e2a06ce2e6f4806da2923
Karl D. Gordon <kgordon@stsci.edu>
2023-11-28 07:45:15 -0500

    removing unneeded imports

--- dust_extinction/__init__.py
+++ dust_extinction/__init__.py
@@ -1,9 +1,2 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from .version import version as __version__
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *   # noqa
-# ----------------------------------------------------------------------------
-
-__all__ = []
