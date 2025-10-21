a41a81103716cb8135e6431963d8827c7ba1afc6
Karl Gordon <kgordon@stsci.edu>
2025-10-21 14:44:14 -0400

    updates and exclude from testing

--- dust_extinction/utils/save_to_ascii.py
+++ dust_extinction/utils/save_to_ascii.py
@@ -6,7 +6,10 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
-    Rvs = np.array(range(34)) * 0.1 + 2.3
+    Rv_range = [2.0, 6.51]
+    n_Rvs = int(np.round((Rv_range[1] - Rv_range[0]) / 0.1) + 1.0)
+    print("# Rvs", n_Rvs)
+    Rvs = np.array(range(n_Rvs)) * 0.1 + Rv_range[0]
     print(Rvs)
     # Rvs = [2.3, 2.7, 3.1, 3.5, 4.0, 4.5, 5.0, 5.6]
     waves = np.logspace(np.log10(0.0912), np.log10(32.), num=1000) * u.micron
@@ -18,7 +21,12 @@ if __name__ == '__main__':
     otab.meta["keywords"]["author"] = {"value": "Karl D. Gordon"}
     otab["wavelength"] = waves
     for cRv in Rvs:
-        cmod = G23(Rv=cRv)
+        cmod = G23()
+        # override Rv_range to allow extrapolation - here be dragons
+        #  Above Rv=6.3, FUV extinction becomes lower at shorter wavelengths
+        #  Below Rv=2.2, MIR before 10um silicates goes unrealistically small
+        cmod.Rv_range = Rv_range
+        cmod.Rv = cRv
         otab.meta["keywords"]["Rv"] = {"value": cRv}
         otab["AlambdaAv"] = cmod(waves)
         otab.write(f"dust_extinction_G23_Rv_{cRv:.1f}.ipac", format="ascii.ipac", overwrite=True)
